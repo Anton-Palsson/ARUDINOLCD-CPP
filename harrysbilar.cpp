fd92a6b75f64b795e0293fd11d57c0a3abfb8fb5
LeoSvahn <145968827+LeoSvahn@users.noreply.github.com>
2023-11-29 12:43:49 +0100

    Update harrysbilar.cpp

--- harrysbilar.cpp
+++ harrysbilar.cpp
@@ -1,1 +1,15 @@
+#include "lcd.h"
+
+// Funktion för att visa information om Kund 1
+void displayCustomerInfo(const Customer& customer) {
+    lcd.Clear();
+    lcd.GoTo(0, 0);
+    lcd.WriteText(customer.name);
+    lcd.GoTo(0, 1);
+    lcd.WriteText(getNextText(customer.textOptions));
+}
+
+const char* getNextText(const char* textOptions[2]) {
+    return (rand() % 2 == 0) ? textOptions[0] : textOptions[1];
+}
 
