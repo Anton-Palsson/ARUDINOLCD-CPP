23958377b77b9a2c191d9e415700ec9b9cebc86f
Anton-Palsson <145968598+Anton-Palsson@users.noreply.github.com>
2023-12-08 12:16:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,166 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "lcd.h"
+#include <stdlib.h>
+#include <string.h>
 
-#define LED_PIN 2
+struct Customer {
+    const char* name;
+    int frequency;
+};
 
-int main(void) {
-    HD44780 lcd;
-    lcd.Initalize();
-    lcd.GoTo(0, 0);
-    lcd.WriteText("HEEEEEEJSAN ");
+const Customer customers[] = {
+    {" Harrys bilar ", 9},
+    {" Farmor Anka ", 4},
+    {" Knegar Petter ", 6},
+    {" Detektivbyra ", 8},
+    {" Virusskydd ", 1}
+};
 
-    char text[50];
-    return 0;
-}
+const int numCustomers = sizeof(customers) / sizeof(customers[0]);
 
+HD44780 lcd;
 
-//SCROLL SOM FUNKAR:
-#include <avr/io.h>
-#include <util/delay.h>
-#include "lcd.h"
-
-int main(void) {
-    HD44780 lcd;
-    lcd.Initalize();
-
-    char text[] = "HEEEEEEJSAN ";
-    int textLength = 12;
+void scrollText(const char* text, int durationInSeconds) {
+    int textLength = strlen(text);
     int displayWidth = 16;
-    int scrollSpeed = 500;  // Justera för önskad scrollhastighet
-    int delayBetweenScrolls = 2000;  // Justera för väntetid innan nästa scrollning
+    int scrollSpeed = 300;
 
-    while (1) {
-        for (int i = 0; i <= textLength + displayWidth; i++) {
-            lcd.Clear(); // Nollställ hela skärmen innan du skriver ny text
+    int iterations = (durationInSeconds * 1000) / (textLength + displayWidth * scrollSpeed);
 
-            if (i < displayWidth) {
-                lcd.GoTo(displayWidth - i, 0);
-                lcd.WriteText(text + i);
-            } else {
-                lcd.GoTo(0, 0);  // Återgå till början av första raden
-                lcd.WriteText(text);
-            }
+    for (int j = 0; j < iterations; j++) {
+        for (int i = 0; i < textLength; i++) {  // Ändra här
+            lcd.Clear();
+
+            lcd.GoTo(displayWidth - i, 0);
+            lcd.WriteText(text + i);
 
             _delay_ms(scrollSpeed);
         }
-        _delay_ms(delayBetweenScrolls);
+    }
+}
+
+void staticText(const char* text) {
+    lcd.Clear();
+    lcd.GoTo(0, 0);
+    lcd.WriteText(text);
+    _delay_ms(20000);
+}
+
+void svartePetter(){
+   lcd.Initalize();
+
+    int randomValue = rand() % 2;
+
+    if (randomValue == 0) {
+        scrollText("Lat Petter bygga åt dig ", 20);
+    } else {
+        staticText("Bygga svart?                            Ring Petter ");
     }
 
+}
+
+void virusSkydd(void){
+     lcd.Initalize();
+
+    int randomValue = rand() % 2;
+
+    if (randomValue == 0) {
+        scrollText("Synas här? IOT:s Reklambyrå", 10);
+}
+}
+
+void farmorAnka() {
+    lcd.Initalize();
+
+    int randomValue = rand() % 2;
+
+    if (randomValue == 0) {
+        scrollText("Kop paj hos Farmor Anka ", 20);
+    } else {
+        staticText("Skynda innan                            Leo tar all paj ");
+    }
+}
+
+void harrysBilar() {
+    lcd.Initalize();
+
+    int randomValue = rand() % 2;
+
+    if (randomValue == 0) {
+        scrollText("Kop bil hos Harry ", 20);
+    } else {
+        staticText("En god bilaffar                         (for Harry!) ");
+    }
+}
+
+void detektivByra() {
+    lcd.Initalize();
+
+    int randomValue = rand() % 2;
+
+    if (randomValue == 0) {
+        staticText("Mysterier?                              Ring Langben! ");
+    } else {
+        staticText("Langben fixar                           biffen ");
+    }
+}
+
+void displayCustomer(const char* customer) {
+    lcd.Clear();
+    lcd.GoTo(0, 0);
+    lcd.WriteText(customer);
+}
+
+int getNextCustomerIndex(int currentIndex) {
+    int totalFrequency = 0;
+
+    for (int i = 0; i < numCustomers; ++i) {
+        totalFrequency += customers[i].frequency;
+    }
+
+    int randomValue = rand() % totalFrequency + 1;
+
+    for (int i = 0; i < numCustomers; ++i) {
+        randomValue -= customers[i].frequency;
+        if (randomValue <= 0 && i != currentIndex) {
+            return i;
+        }
+    }
+
+    return getNextCustomerIndex(currentIndex);
+}
+
+void startProgram(void) {
+    lcd.Initalize();
+
+    int currentIndex = -1;
+    int nextIndex;
+
+    while (1) {
+        nextIndex = getNextCustomerIndex(currentIndex);
+
+        currentIndex = nextIndex;
+
+        displayCustomer(customers[nextIndex].name);
+
+        if (strcmp(customers[nextIndex].name, " Harrys bilar ") == 0) {
+            harrysBilar();
+        } else if (strcmp(customers[nextIndex].name, " Farmor Anka ") == 0) {
+            farmorAnka();
+        } else if (strcmp(customers[nextIndex].name, " Knegar Petter ") == 0) {
+            svartePetter();
+        } else if (strcmp(customers[nextIndex].name, " Detektivbyra ") == 0) {
+            detektivByra();
+        } else if (strcmp(customers[nextIndex].name, " Virusskydd ") == 0) {
+            virusSkydd();
+        }
+
+      
+    }
+}
+
+int main(void) {
+    startProgram();
     return 0;
 }
